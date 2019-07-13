7bddad0004905eecf82a6acf8190e83b786ad60e
inPhoenix <spiderk@gmail.com>
2019-07-13 10:59:27 +0100

    Updates

---
+++ precache-manifest.eecef2e1029fb0b4a320b6fb99145b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df4851298a4df16dfc71",
+    "url": "/inMapPins/static/js/main.df485129.chunk.js"
+  },
+  {
+    "revision": "5f18da34f9c4ec874fe2",
+    "url": "/inMapPins/static/css/1.8011ae36.chunk.css"
+  },
+  {
+    "revision": "5f18da34f9c4ec874fe2",
+    "url": "/inMapPins/static/js/1.5f18da34.chunk.js"
+  },
+  {
+    "revision": "73b882ca3bd0c318e5de",
+    "url": "/inMapPins/static/js/runtime~main.73b882ca.js"
+  },
+  {
+    "revision": "bf4e1ce59eef0ea516c45eba307aa13c",
+    "url": "/inMapPins/index.html"
+  }
+];
